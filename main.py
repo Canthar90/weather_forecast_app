a776ef956e4f6f71eed55c7874a771c32f665ffe
Canthar90 <d.cieslak90@gmail.com>
2023-01-09 12:06:54 +0100

    Added layout

---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import plotly.express as px
+
+
+st.set_page_config(
+    page_title="Weather forecast",
+    page_icon="☀️"
+)
+
+
+st.title("Weather Forecast for the Next Days")
+
+place = st.text_input("Place: ")
+
+slider = st.slider(
+    "Number of days",
+    min_value=1, max_value=5,
+    help="Select numer of forecasted days"
+) 
+option = st.selectbox("Sekect data to view", ("Temperature", "Sky"))
+
+st.subheader(f"{option} for next {slider} days in {place} ")
+
+dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
+temperatures = [10, 11, 15]
+temperatures = [slider * i for i in temperatures]
+figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
